f34bcd64bd471c7ad9b0cabb47b7f6ad530e27ee
saravananganesan31 <41565684+saravananganesan31@users.noreply.github.com>
2020-02-07 20:35:36 -0800

    Update myPython.py

--- myPython.py
+++ myPython.py
@@ -22,4 +22,24 @@ x=int(input("enter no"))
 y=factorial(x)
 print(y),     
 ----------------------------------------- 
+# With a given integral number n, write a program to generate a dictionary that contains (i, i*i) such that is an integral number between 1 and n (both included). and then the program should print the dictionary.
+# Suppose the following input is supplied to the program:
+# 8
+# Then, the output should be:
+# {1: 1, 2: 4, 3: 9, 4: 16, 5: 25, 6: 36, 7: 49, 8: 64}
+
+x=int(input("enter no : "))
+print(x)
+d=dict()
+for i in range(1,x+1):
+            d[i]=i*i
+print(d)
+
+enter no : 10
+10
+{1: 1, 2: 4, 3: 9, 4: 16, 5: 25, 6: 36, 7: 49, 8: 64, 9: 81, 10: 100}
+----------------------------------------- 
+
+
+
 
