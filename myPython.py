ee3f167b2e0f23e6bfc794b0e3576ab46973c4f0
saravananganesan31 <41565684+saravananganesan31@users.noreply.github.com>
2020-02-08 20:54:51 -0800

    Update myPython.py

--- myPython.py
+++ myPython.py
@@ -57,5 +57,22 @@ output:
 ['1', '2', '3', '4', '5']
 ('1', '2', '3', '4', '5')
 ----------------------------------------- 
+# Given an integer, , perform the following conditional actions:
+# If  is odd, print Weird
+# If  is even and in the inclusive range of  to , print Not Weird
+# If  is even and in the inclusive range of  to , print Weird
+# If  is even and greater than , print Not Weird
 
+#!/bin/python3
+n = int(input().strip())
+a=n%2.0
+if (a>=1):
+    print("Weird")
+elif (a==0) and n>=2 and n<=5:
+    print("Not Weird")
+elif (a==0) and n>=6 and n<=20:
+    print("Weird")
+elif n>20:
+    print("Not Weird")
+----------------------------------------- 
 
