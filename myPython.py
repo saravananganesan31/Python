c8a3facd4860e64b2201221ea5bfbda306943f3f
saravananganesan31 <41565684+saravananganesan31@users.noreply.github.com>
2020-02-08 21:02:57 -0800

    Update myPython.py

--- myPython.py
+++ myPython.py
@@ -89,4 +89,16 @@ print(c)
 print(d)
 print(e)
 ----------------------------------------- 
+# Read two integers and print two lines. The first line should contain integer division,  // . The second line should contain float division,  / .
+# You don't need to perform any rounding or formatting operations.
+# Input Format
+# The first line contains the first integer, . The second line contains the second integer, .
 
+a=int(input().strip())
+b=int(input().strip())
+c=int(a/b)
+d=float(a/b)
+print(c)
+print(d)
+
+----------------------------------------- 
