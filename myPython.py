0e20a8df9a6045893d3e6367865bccb34c568dc6
saravananganesan31 <41565684+saravananganesan31@users.noreply.github.com>
2020-02-09 02:21:40 -0800

    Update myPython.py

--- myPython.py
+++ myPython.py
@@ -152,7 +152,27 @@ for i in range(a):
     print(i+1,end='')
    
 ----------------------------------------- 
+# zeros
+# The zeros tool returns a new array with a given shape and type filled with 's.
+# import numpy
+# print numpy.zeros((1,2))                    #Default type is float
+# Output : [[ 0.  0.]] 
+# print numpy.zeros((1,2), dtype = numpy.int) #Type changes to int
+# Output : [[0 0]]
+# ones
+# The ones tool returns a new array with a given shape and type filled with 's.
+# print numpy.ones((1,2))                    #Default type is float
+# Output : [[ 1.  1.]] 
+# print numpy.ones((1,2), dtype = numpy.int) #Type changes to int
+# Output : [[1 1]]   
+# Task - You are given the shape of the array in the form of space-separated integers, each integer representing the size of different dimensions, your task is to print an array of the given shape and integer type using the tools numpy.zeros and numpy.ones.
 
-
+import numpy
+N = tuple(map(int,input().strip().split()))
+z = numpy.zeros(N, dtype=numpy.int)
+o = numpy.ones(N, dtype=numpy.int)
+print (z)
+print (o)
+----------------------------------------- 
 
 
